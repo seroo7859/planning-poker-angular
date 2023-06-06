afaf05ac059128815d78d116a2203470a23f8371
seroo7859 <104740228+seroo7859@users.noreply.github.com>
2023-06-06 20:47:40 +0200

    add planning poker store effects and index

---
+++ src/app/planning-poker-store/effects/index.ts
@@ -0,0 +1,9 @@
+import * as SessionEffects from './session.effects';
+import * as UserEffects from './user.effects';
+import * as TeamEffects from './team.effects';
+
+export {
+  SessionEffects,
+  UserEffects,
+  TeamEffects
+};
---
+++ src/app/planning-poker-store/effects/planning-poker-store.effects.ts
@@ -0,0 +1,7 @@
+import { SessionEffects, TeamEffects, UserEffects } from "./index";
+
+export const effects = [
+  SessionEffects.SessionEffects,
+  UserEffects.UserEffects,
+  TeamEffects.TeamEffects
+];
