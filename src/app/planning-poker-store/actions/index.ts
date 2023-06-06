e05c70e0363f570e8b51c7c60bec59455afd979d
seroo7859 <104740228+seroo7859@users.noreply.github.com>
2023-06-06 20:32:25 +0200

    add planning poker store action types and index

---
+++ src/app/planning-poker-store/actions/index.ts
@@ -0,0 +1,11 @@
+import * as DecksActions from './decks.actions';
+import * as SessionActions from './session.actions';
+import * as UserActions from './user.actions';
+import * as TeamActions from './team.actions';
+
+export {
+  DecksActions,
+  SessionActions,
+  UserActions,
+  TeamActions
+};
---
+++ src/app/planning-poker-store/actions/planning-poker-store.actions.ts
@@ -0,0 +1,28 @@
+import {getCurrentUserFailure, getCurrentUserSuccess} from "./user.actions";
+
+export enum ActionTypes {
+  AddDeck = '[Decks] Add Deck',
+
+  CreateSession        = '[Session] Create Session',
+  CreateSessionSuccess = '[Session] Create Session Success',
+  CreateSessionFailure = '[Session] Create Session Failure',
+
+  JoinSession        = '[Session] Join Session',
+  JoinSessionSuccess = '[Session] Join Session Success',
+  JoinSessionFailure = '[Session] Join Session Failure',
+
+  GetSession        = '[Session] Get Session',
+  GetSessionSuccess = '[Session] Get Session Success',
+  GetSessionFailure = '[Session] Get Session Failure',
+
+  GetCurrentUser          = '[User] Get Current User',
+  GetCurrentUserSuccess   = '[User] Get Current User Success',
+  GetCurrentUserFailure   = '[User] Get Current User Failure',
+  CurrentUserConnected    = '[User] Current User Connected',
+  CurrentUserDisconnected = '[User] Current User Disconnected',
+
+  TeamMemberJoined       = '[Team] Team Member Joined',
+  TeamMemberLeaved       = '[Team] Team Member Leaved',
+  TeamMemberConnected    = '[Team] Team Member Connected',
+  TeamMemberDisconnected = '[Team] Team Member Disconnected'
+}
