425c403a2261b95eb31828ef91ec12367d6e0493
seroo7859 <104740228+seroo7859@users.noreply.github.com>
2023-06-06 21:12:44 +0200

    import forms module on onboarding module

--- src/app/features/onboarding/onboarding.module.ts
+++ src/app/features/onboarding/onboarding.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from "@angular/forms";
 
 import { OnboardingRoutingModule } from './onboarding-routing.module';
 import { OnboardingComponent } from './onboarding.component';
 import { CreateSessionComponent } from './create-session/create-session.component';
 import { JoinSessionComponent } from './join-session/join-session.component';
+import { NgbTooltipModule } from "@ng-bootstrap/ng-bootstrap";
+import { SharedModule } from "../../shared/shared.module";
+import { CoreModule } from "../../core/core.module";
 
 
 @NgModule({
@@ -13,9 +17,13 @@ import { JoinSessionComponent } from './join-session/join-session.component';
     CreateSessionComponent,
     JoinSessionComponent
   ],
-  imports: [
-    CommonModule,
-    OnboardingRoutingModule
-  ]
+    imports: [
+        CommonModule,
+        FormsModule,
+        OnboardingRoutingModule,
+        NgbTooltipModule,
+        SharedModule,
+        CoreModule
+    ]
 })
 export class OnboardingModule { }
