bb90af400b51e68c7241a1278d2634640d1ca7c0
seroo7859 <104740228+seroo7859@users.noreply.github.com>
2023-06-06 20:12:31 +0200

    add create, join and get session as service methods

--- src/app/core/services/session.service.ts
+++ src/app/core/services/session.service.ts
@@ -1,15 +1,58 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { delay, Observable, tap } from "rxjs";
+import { SessionCreateModel, SessionJoinModel, SessionModel } from "../models/session.model";
+import { AuthService } from "./auth.service";
+import { map } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class SessionService {
 
-  constructor() { }
+  // URL to Session-API
+  private url = 'api/session';
 
-  isJoinedSession() {
-    // Todo: implement
-    return false;
+  // HTTP Options
+  private httpOptions = {
+    headers: new HttpHeaders({ "Content-Type": "application/json" })
+  };
+
+  constructor(private http: HttpClient, private authService: AuthService) {}
+
+  /**
+   * Create a new session on the server.
+   * @param session
+   */
+  createSession(session: SessionCreateModel): Observable<SessionModel> {
+    return this.http.post<SessionModel>(this.url, session, { observe: 'response', ...this.httpOptions })
+      .pipe(
+        delay(500),
+        tap(response => this.authService.handleResponse(response)),
+        map(response => response.body as SessionModel)
+      );
+  }
+
+  /**
+   * Join to a session on the server.
+   * @param session
+   */
+  joinSession(session: SessionJoinModel): Observable<SessionModel> {
+    const { sessionId, ...data } = session;
+    return this.http.post<SessionModel>(`${this.url}/${session.sessionId}/join`, data, { observe: 'response', ...this.httpOptions })
+      .pipe(
+        delay(500),
+        tap(response => this.authService.handleResponse(response)),
+        map(response => response.body as SessionModel)
+      );
+  }
+
+  /**
+   * Get a session by public id from server.
+   * @param sessionId
+   */
+  getSession(sessionId: string): Observable<SessionModel> {
+    return this.http.get<SessionModel>(`${this.url}/${sessionId}`, this.httpOptions);
   }
 
 }
