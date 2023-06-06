a80ea861c48fa92ef2be3f0e11768a947e39e0b5
seroo7859 <104740228+seroo7859@users.noreply.github.com>
2023-06-06 21:05:01 +0200

    refactor planning poker store module

--- src/app/planning-poker-store/planning-poker-store.module.ts
+++ src/app/planning-poker-store/planning-poker-store.module.ts
@@ -3,15 +3,17 @@ import { CommonModule } from '@angular/common';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { metaReducers, reducers } from "./state/planning-poker.state";
+import { metaReducers, reducers } from "./state/planning-poker-store.state";
+import { effects } from "./effects/planning-poker-store.effects";
+import { environment } from "../../environments/environment";
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     StoreModule.forRoot(reducers, { metaReducers }),
-    EffectsModule.forRoot([]),
-    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() })
+    EffectsModule.forRoot(effects),
+    !environment.production ? StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }) : []
   ]
 })
 export class PlanningPokerStoreModule { }
