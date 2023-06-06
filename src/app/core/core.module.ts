9aa31955a2fd081358666e55d0734a62661ca386
seroo7859 <104740228+seroo7859@users.noreply.github.com>
2023-06-06 22:01:40 +0200

    add rx-stomp service

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,53 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from "@angular/forms";
+import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
+import { NgbToastModule, NgbTooltipModule } from "@ng-bootstrap/ng-bootstrap";
+import { CreateDeckModalContentComponent } from './components/create-deck-modal-content/create-deck-modal-content.component';
+import { DeckCardComponent } from './components/deck/deck-card/deck-card.component';
+import { DeckComponent } from './components/deck/deck.component';
+import { DeckInputValidatorDirective } from './directives/deck-input-validator.directive';
+import { SessionIdValidatorDirective } from './directives/session-id-validator.directive';
+import { AuthInterceptor } from "./interceptors/auth.interceptor";
+import { LoggingInterceptor } from "./interceptors/logging.interceptor";
+import { RxStompService, rxStompServiceFactory } from "./services/rx-stomp.service";
+import { ToastsComponent } from './components/toasts/toasts.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CreateDeckModalContentComponent,
+    DeckCardComponent,
+    DeckComponent,
+    DeckInputValidatorDirective,
+    SessionIdValidatorDirective,
+    ToastsComponent
+  ],
+  exports: [
+    SessionIdValidatorDirective,
+    ToastsComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    HttpClientModule,
+    NgbTooltipModule,
+    NgbToastModule
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LoggingInterceptor,
+      multi: true
+    },
+    {
+      provide: RxStompService,
+      useFactory: rxStompServiceFactory
+    }
   ]
 })
 export class CoreModule { }
---
+++ src/app/core/services/rx-stomp.service.ts
@@ -0,0 +1,67 @@
+import { inject, Injectable } from '@angular/core';
+import { RxStomp, RxStompConfig, RxStompState } from "@stomp/rx-stomp";
+import { environment } from "../../../environments/environment";
+import { AuthService } from "./auth.service";
+import { filter, Observable } from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RxStompService extends RxStomp {
+
+  public readonly disconnected$: Observable<RxStompState>;
+
+  constructor() {
+    super();
+    this.disconnected$ = this.connectionState$.pipe(
+      filter((currentState: RxStompState) => {
+        return currentState === RxStompState.CLOSED;
+      })
+    );
+  }
+
+  public disconnected(): boolean {
+    return this.connectionState$.getValue() === RxStompState.CLOSED;
+  }
+}
+
+export function rxStompServiceFactory() {
+  const rxStomp = new RxStompService();
+  rxStomp.configure(myRxStompConfig);
+  rxStomp.connected$.subscribe(() => {
+    console.log('I will be called for each time connection is established.');
+  });
+  rxStomp.disconnected$.subscribe(() => {
+    console.log('I will be called for each time connection is breaks down.');
+  });
+  rxStomp.activate();
+  return rxStomp;
+}
+
+const myRxStompConfig: RxStompConfig = {
+  brokerURL: environment.brokerUrl,   // server
+
+  connectHeaders: {
+    token: 'my-token'                 // Access token
+  },
+
+  heartbeatIncoming: 0,               // disabled
+  heartbeatOutgoing: 20000,           // every 20 seconds
+
+  connectionTimeout: 0,               // wait forever
+  reconnectDelay: 200,                // wait 200 milliseconds before attempting auto reconnect
+
+  debug: !environment.production ? log : undefined,    // log diagnostics
+
+  beforeConnect: (client) => {
+    const authService = inject(AuthService);
+    const authToken = authService.getAuthorizationToken();
+    if (authToken) {
+      client.configure({ ...myRxStompConfig, connectHeaders: { token: authToken } });
+    }
+  }
+};
+
+function log(msg: string): void {
+  console.log(new Date(), msg);
+}
