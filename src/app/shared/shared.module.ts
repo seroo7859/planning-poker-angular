cc2f9b7fd29c6fa72be6abc2090619f3132a4740
seroo7859 <104740228+seroo7859@users.noreply.github.com>
2023-06-06 21:43:07 +0200

    add join pipe to shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { JoinPipe } from './pipes/join.pipe';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    JoinPipe
+  ],
+  exports: [
+    JoinPipe
+  ],
   imports: [
     CommonModule
   ]
