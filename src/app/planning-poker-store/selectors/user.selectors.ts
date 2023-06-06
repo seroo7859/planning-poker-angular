145876b8d3e2055dc18bd02b6e2e307bed744169
seroo7859 <104740228+seroo7859@users.noreply.github.com>
2023-06-06 21:01:06 +0200

    add user selectors

---
+++ src/app/planning-poker-store/selectors/user.selectors.ts
@@ -0,0 +1,7 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { PlanningPokerStoreStateModel } from "../state/model/planning-poker-store.state.model";
+import { storeFeatureKey } from "../reducers/planning-poker-store.reducers";
+
+const selectPlanningPokerStoreState = createFeatureSelector<PlanningPokerStoreStateModel>(storeFeatureKey);
+
+export const selectUser = createSelector(selectPlanningPokerStoreState, (state: PlanningPokerStoreStateModel) => state.user);
