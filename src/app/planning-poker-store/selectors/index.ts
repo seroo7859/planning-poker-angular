3269c98972d682c72ae3a4cb2a7660b7b7eb0df0
seroo7859 <104740228+seroo7859@users.noreply.github.com>
2023-06-06 21:03:07 +0200

    add selectors to index

---
+++ src/app/planning-poker-store/selectors/index.ts
@@ -0,0 +1,9 @@
+import * as DecksSelectors from './decks.selectors';
+import * as SessionSelectors from './session.selectors';
+import * as UserSelectors from './user.selectors';
+
+export {
+  DecksSelectors,
+  SessionSelectors,
+  UserSelectors
+};
