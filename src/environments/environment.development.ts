4e5577839df7c8d5b4a2b17441e395e6beb586f1
seroo7859 <104740228+seroo7859@users.noreply.github.com>
2023-06-06 19:44:13 +0200

    add websocket to proxy configuration

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   name: 'dev',
-  apiUrl: 'http://localhost:8080'
+  apiUrl: 'http://localhost:8080/api',
+  brokerUrl: 'ws://localhost:8080/ws'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   name: 'prod',
-  apiUrl: null
+  apiUrl: '',
+  brokerUrl: ''
 };
