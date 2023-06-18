7e2b3b09c2c7330e40e2e18c67cf91aa2d311653
seroo7859 <104740228+seroo7859@users.noreply.github.com>
2023-06-18 21:37:29 +0200

    add backlog action types

--- src/app/planning-poker-store/actions/planning-poker-store.actions.ts
+++ src/app/planning-poker-store/actions/planning-poker-store.actions.ts
@@ -22,5 +22,44 @@ export enum ActionTypes {
   TeamMemberJoined       = '[Team] Team Member Joined',
   TeamMemberLeaved       = '[Team] Team Member Leaved',
   TeamMemberConnected    = '[Team] Team Member Connected',
-  TeamMemberDisconnected = '[Team] Team Member Disconnected'
+  TeamMemberDisconnected = '[Team] Team Member Disconnected',
+
+  ImportBacklog        = '[Backlog] Import Backlog',
+  ImportBacklogSuccess = '[Backlog] Import Backlog Success',
+  ImportBacklogFailure = '[Backlog] Import Backlog Failure',
+  BacklogImported      = '[Backlog] Backlog Imported',
+
+  ExportBacklog        = '[Backlog] Export Backlog',
+  ExportBacklogSuccess = '[Backlog] Export Backlog Success',
+  ExportBacklogFailure = '[Backlog] Export Backlog Failure',
+
+  RenameBacklog        = '[Backlog] Rename Backlog ',
+  RenameBacklogSuccess = '[Backlog] Rename Backlog Success',
+  RenameBacklogFailure = '[Backlog] Rename Backlog Failure',
+  BacklogRenamed       = '[Backlog] Backlog Renamed',
+
+  ClearBacklog        = '[Backlog] Clear Backlog',
+  ClearBacklogSuccess = '[Backlog] Clear Backlog Success',
+  ClearBacklogFailure = '[Backlog] Clear Backlog Failure',
+  BacklogCleared      = '[Backlog] Backlog Cleared',
+
+  AddBacklogItem        = '[Backlog] Add Backlog Item',
+  AddBacklogItemSuccess = '[Backlog] Add Backlog Item Success',
+  AddBacklogItemFailure = '[Backlog] Add Backlog Item Failure',
+  BacklogItemAdded      = '[Backlog] Backlog Item Added',
+
+  RemoveBacklogItem        = '[Backlog] Remove Backlog Item',
+  RemoveBacklogItemSuccess = '[Backlog] Remove Backlog Item Success',
+  RemoveBacklogItemFailure = '[Backlog] Remove Backlog Item Failure',
+  BacklogItemRemoved       = '[Backlog] Backlog Item Removed',
+
+  UpdateBacklogItem        = '[Backlog] Updated Backlog Item',
+  UpdateBacklogItemSuccess = '[Backlog] Updated Backlog Item Success',
+  UpdateBacklogItemFailure = '[Backlog] Updated Backlog Item Failure',
+  BacklogItemUpdated       = '[Backlog] Backlog Item Updated',
+
+  MoveBacklogItem        = '[Backlog] Move Backlog Item',
+  MoveBacklogItemSuccess = '[Backlog] Move Backlog Item Success',
+  MoveBacklogItemFailure = '[Backlog] Move Backlog Item Failure',
+  BacklogItemMoved       = '[Backlog] Backlog Item Moved'
 }
