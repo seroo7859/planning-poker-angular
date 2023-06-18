da21ecc5c57596731db46892436e414150affda5
seroo7859 <104740228+seroo7859@users.noreply.github.com>
2023-06-18 21:53:13 +0200

    add backlog, estimate and discuss component to dashboard component

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from "@ngrx/store";
-import { Observable, Subscription, take } from "rxjs";
-import { SessionSelectors } from "../../planning-poker-store/selectors";
+import { Observable, Subscription, take, withLatestFrom } from "rxjs";
+import { SessionSelectors, UserSelectors } from "../../planning-poker-store/selectors";
 import { RxStompService } from "../../core/services/rx-stomp.service";
-import { TeamActions, UserActions } from "../../planning-poker-store/actions";
+import { BacklogActions, TeamActions, UserActions } from "../../planning-poker-store/actions";
+import { UserRoleModel } from "../../core/models/user.model";
 
 @Component({
   selector: 'app-dashboard',
@@ -12,51 +13,115 @@ import { TeamActions, UserActions } from "../../planning-poker-store/actions";
 })
 export class DashboardComponent implements OnInit, OnDestroy {
 
+  isBacklogCollapsed: boolean = false;
+  isDiscussCollapsed: boolean = false;
+
   session$: Observable<any> = new Observable<any>();
+  user$: Observable<any> = new Observable<any>();
+
   private sessionSubscription: Subscription = new Subscription();
 
   constructor(private readonly store: Store, private rxStompService: RxStompService) {}
 
   ngOnInit(): void {
     this.session$ = this.store.select(SessionSelectors.selectSession);
-    this.sessionSubscription.add(
-      this.session$.pipe(take(1))
-        .subscribe(session => {
-          if (session.data) {
-            this.subscribeSession(session.data.id);
-          }
-      })
-    );
+    this.user$ = this.store.select(UserSelectors.selectUser);
+    this.subscribeSession();
   }
 
-  private subscribeSession(sessionId: string) {
+  private subscribeSession() {
     this.sessionSubscription.add(
-      this.rxStompService
-        .watch(`/session/${sessionId}/team/member-joined`)
-        .subscribe((message) => this.store.dispatch(TeamActions.teamMemberJoined({ teamMember: JSON.parse(message.body) })))
-    );
-    this.sessionSubscription.add(
-      this.rxStompService
-        .watch(`/session/${sessionId}/team/member-leaved`)
-        .subscribe((message) => this.store.dispatch(TeamActions.teamMemberLeaved({ teamMember: JSON.parse(message.body) })))
-    );
-    this.sessionSubscription.add(
-      this.rxStompService
-        .watch(`/session/${sessionId}/team/member-connected`)
-        .subscribe((message) => this.store.dispatch(TeamActions.teamMemberConnected({ teamMember: JSON.parse(message.body) })))
-    );
-    this.sessionSubscription.add(
-      this.rxStompService
-        .watch(`/session/${sessionId}/team/member-disconnected`)
-        .subscribe((message) => this.store.dispatch(TeamActions.teamMemberDisconnected({ teamMember: JSON.parse(message.body) })))
-    );
-    this.sessionSubscription.add(
-      this.rxStompService.connected$
-        .subscribe(() => this.store.dispatch(UserActions.currentUserConnected()))
-    );
-    this.sessionSubscription.add(
-      this.rxStompService.disconnected$
-        .subscribe(() => this.store.dispatch(UserActions.currentUserDisconnected()))
+      this.session$
+        .pipe(
+          take(1),
+          withLatestFrom(this.user$)
+        )
+        .subscribe(([session, user]) => {
+          if (session.data && user) {
+            const sessionId: string = session.data.id;
+
+            this.sessionSubscription.add(
+              this.rxStompService
+                .watch(`/session/${sessionId}/team/member-joined`)
+                .subscribe((message) => this.store.dispatch(TeamActions.teamMemberJoined({ teamMember: JSON.parse(message.body) })))
+            );
+
+            this.sessionSubscription.add(
+              this.rxStompService
+                .watch(`/session/${sessionId}/team/member-leaved`)
+                .subscribe((message) => this.store.dispatch(TeamActions.teamMemberLeaved({ teamMember: JSON.parse(message.body) })))
+            );
+
+            this.sessionSubscription.add(
+              this.rxStompService
+                .watch(`/session/${sessionId}/team/member-connected`)
+                .subscribe((message) => this.store.dispatch(TeamActions.teamMemberConnected({ teamMember: JSON.parse(message.body) })))
+            );
+
+            this.sessionSubscription.add(
+              this.rxStompService
+                .watch(`/session/${sessionId}/team/member-disconnected`)
+                .subscribe((message) => this.store.dispatch(TeamActions.teamMemberDisconnected({ teamMember: JSON.parse(message.body) })))
+            );
+
+            if(user.role !== UserRoleModel.MODERATOR) {
+              this.sessionSubscription.add(
+                this.rxStompService
+                  .watch(`/session/${sessionId}/backlog/imported`)
+                  .subscribe((message) => this.store.dispatch(BacklogActions.backlogImported({ backlog: JSON.parse(message.body) })))
+              );
+
+              this.sessionSubscription.add(
+                this.rxStompService
+                  .watch(`/session/${sessionId}/backlog/renamed`)
+                  .subscribe((message) => this.store.dispatch(BacklogActions.backlogRenamed({ name: JSON.parse(message.body).name })))
+              );
+
+              this.sessionSubscription.add(
+                this.rxStompService
+                  .watch(`/session/${sessionId}/backlog/cleared`)
+                  .subscribe((message) => this.store.dispatch(BacklogActions.backlogCleared({ backlog: JSON.parse(message.body) })))
+              );
+
+              this.sessionSubscription.add(
+                this.rxStompService
+                  .watch(`/session/${sessionId}/backlog/item-added`)
+                  .subscribe((message) => this.store.dispatch(BacklogActions.backlogItemAdded({ backlogItem: JSON.parse(message.body) })))
+              );
+
+              this.sessionSubscription.add(
+                this.rxStompService
+                  .watch(`/session/${sessionId}/backlog/item-removed`)
+                  .subscribe((message) => this.store.dispatch(BacklogActions.backlogItemRemoved({ backlogItem: JSON.parse(message.body) })))
+              );
+
+              this.sessionSubscription.add(
+                this.rxStompService
+                  .watch(`/session/${sessionId}/backlog/item-updated`)
+                  .subscribe((message) => this.store.dispatch(BacklogActions.backlogItemUpdated({ backlogItem: JSON.parse(message.body) })))
+              );
+
+              this.sessionSubscription.add(
+                this.rxStompService
+                  .watch(`/session/${sessionId}/backlog/item-moved`)
+                  .subscribe((message) => {
+                    const result = JSON.parse(message.body);
+                    this.store.dispatch(BacklogActions.backlogItemMoved({ backlog: result.backlog, backlogItem: result.backlogItem, newIndex: result.newIndex }))
+                  })
+              );
+            }
+
+            this.sessionSubscription.add(
+              this.rxStompService.connected$
+                .subscribe(() => this.store.dispatch(UserActions.currentUserConnected()))
+            );
+
+            this.sessionSubscription.add(
+              this.rxStompService.disconnected$
+                .subscribe(() => this.store.dispatch(UserActions.currentUserDisconnected()))
+            );
+          }
+        })
     );
   }
 
--- src/app/features/dashboard/dashboard.module.ts
+++ src/app/features/dashboard/dashboard.module.ts
@@ -3,18 +3,50 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
-import { NgbToastModule } from "@ng-bootstrap/ng-bootstrap";
+import { NgbCollapseModule, NgbToastModule, NgbTooltipModule  } from "@ng-bootstrap/ng-bootstrap";
 import { CoreModule } from "../../core/core.module";
+import { BacklogComponent } from './backlog/backlog.component';
+import { EstimateComponent } from './estimate/estimate.component';
+import { DiscussComponent } from './discuss/discuss.component';
+import { JDENTICON_CONFIG, NgxJdenticonModule } from "ngx-jdenticon";
+import {FormsModule} from "@angular/forms";
+import {SharedModule} from "../../shared/shared.module";
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    BacklogComponent,
+    EstimateComponent,
+    DiscussComponent
   ],
-  imports: [
-    CommonModule,
-    DashboardRoutingModule,
-    NgbToastModule,
-    CoreModule
-  ]
+    imports: [
+        CommonModule,
+        DashboardRoutingModule,
+        NgbTooltipModule,
+        NgbToastModule,
+        NgbCollapseModule,
+        NgxJdenticonModule,
+        CoreModule,
+        FormsModule,
+        SharedModule
+    ],
+  providers: [
+    {
+      // Custom identicon style
+      // https://jdenticon.com/icon-designer.html?config=864444000141321e24501448
+      provide: JDENTICON_CONFIG,
+      useValue: {
+        lightness: {
+          color: [0.37, 0.81],
+          grayscale: [0.20, 0.72]
+        },
+        saturation: {
+          color: 0.51,
+          grayscale: 0.30
+        },
+        backColor: "#0000"
+      },
+    }
+  ],
 })
 export class DashboardModule { }
