e3cd573c59e595901c08df13bdb2a8a5a956044a
seroo7859 <104740228+seroo7859@users.noreply.github.com>
2023-06-18 21:46:50 +0200

    add team selectors

--- src/app/planning-poker-store/selectors/index.ts
+++ src/app/planning-poker-store/selectors/index.ts
@@ -1,9 +1,15 @@
 import * as DecksSelectors from './decks.selectors';
+import * as DeckSelectors from './deck.selectors';
 import * as SessionSelectors from './session.selectors';
 import * as UserSelectors from './user.selectors';
+import * as TeamSelectors from './team.selectors';
+import * as BacklogSelectors from './backlog.selectors';
 
 export {
   DecksSelectors,
+  DeckSelectors,
   SessionSelectors,
-  UserSelectors
+  UserSelectors,
+  TeamSelectors,
+  BacklogSelectors
 };
---
+++ src/app/planning-poker-store/selectors/team.selectors.spec.ts
@@ -0,0 +1,5 @@
+describe('Team Selectors', () => {
+  it('should select the feature state', () => {
+
+  });
+});
