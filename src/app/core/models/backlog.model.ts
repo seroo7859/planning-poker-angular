6118cb0d5c42e51dec62d27fb17778e98477c6f4
seroo7859 <104740228+seroo7859@users.noreply.github.com>
2023-06-18 21:33:51 +0200

    add backlog model

---
+++ src/app/core/models/backlog.model.ts
@@ -0,0 +1,29 @@
+export interface BacklogModel {
+  name: string;
+  items: BacklogItemModel[]
+}
+
+export interface BacklogItemModel {
+  number: string;
+  title: string;
+  description: string;
+  estimation: string;
+  priority: string;
+}
+
+export interface BacklogItemAddModel {
+  title: string;
+  description: string;
+}
+
+export interface BacklogItemUpdateModel {
+  title: string;
+  description: string;
+  estimation: string;
+  priority: string;
+}
+
+export interface BacklogExportModel {
+  filename: string;
+  data: Blob
+}
