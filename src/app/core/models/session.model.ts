e0763b1923cc6ce69fc10e392e148cc39782efe4
seroo7859 <104740228+seroo7859@users.noreply.github.com>
2023-06-18 21:34:18 +0200

    add backlog to session model

--- src/app/core/models/session.model.ts
+++ src/app/core/models/session.model.ts
@@ -1,10 +1,12 @@
 import { DeckModel } from "./deck.model";
 import { TeamModel } from "./team.model";
+import { BacklogModel } from "./backlog.model";
 
 export interface SessionModel {
   id: string;
   team: TeamModel;
   deck: DeckModel;
+  backlog: BacklogModel;
   createdAt: string;
 }
 
