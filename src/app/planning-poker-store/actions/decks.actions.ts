c22d8d0dc62ba31c62344a800ad868d34a4a1bc9
seroo7859 <104740228+seroo7859@users.noreply.github.com>
2023-06-18 21:36:16 +0200

    refactor decks actions

--- src/app/planning-poker-store/actions/decks.actions.ts
+++ src/app/planning-poker-store/actions/decks.actions.ts
@@ -1,6 +1,7 @@
 import { createAction, props } from '@ngrx/store';
 import { DeckModel } from "../../core/models/deck.model";
-import {ActionTypes} from "./planning-poker-store.actions";
+import { ActionTypes } from "./planning-poker-store.actions";
+
 export const addDeck = createAction(
   ActionTypes.AddDeck,
   props<{ deck: DeckModel }>()
