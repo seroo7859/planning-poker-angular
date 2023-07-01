1f32e0cde867f2b061fb3fabe8c122154bdad777
seroo7859 <104740228+seroo7859@users.noreply.github.com>
2023-07-01 21:28:18 +0200

    add leave session reducer

--- src/app/planning-poker-store/reducers/session.reducers.ts
+++ src/app/planning-poker-store/reducers/session.reducers.ts
@@ -78,6 +78,43 @@ export const joinSessionFailure = on(SessionActions.joinSessionFailure, (state: 
 });
 
 
+// Leave Session Reducers
+
+export const leaveSession = on(SessionActions.leaveSession, (state: PlanningPokerStoreStateModel, action: ActionType<any>) => {
+  const newState: PlanningPokerStoreStateModel = cloneDeep(state);
+  newState.session.xhr = {
+    pending: true,
+    succeed: false,
+    failed: false,
+    error: null
+  };
+  return newState;
+});
+
+export const leaveSessionSuccess = on(SessionActions.leaveSessionSuccess, (state: PlanningPokerStoreStateModel, action: ActionType<any>) => {
+  const newState: PlanningPokerStoreStateModel = cloneDeep(state);
+  newState.session.data = null;
+  newState.session.xhr = {
+    pending: false,
+    succeed: true,
+    failed: false,
+    error: null
+  };
+  return newState;
+});
+
+export const leaveSessionFailure = on(SessionActions.leaveSessionFailure, (state: PlanningPokerStoreStateModel, action: ActionType<any>) => {
+  const newState: PlanningPokerStoreStateModel = cloneDeep(state);
+  newState.session.xhr = {
+    pending: false,
+    succeed: false,
+    failed: true,
+    error: action.error
+  };
+  return newState;
+});
+
+
 // Get Session Reducers
 
 export const getSession = on(SessionActions.getSession, (state: PlanningPokerStoreStateModel, action: ActionType<any>) => {
