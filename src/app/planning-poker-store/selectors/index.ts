83400ded9db625450e9c5a9db69050b9a9070266
seroo7859 <104740228+seroo7859@users.noreply.github.com>
2023-07-01 21:32:52 +0200

    add estimation and discussion selectors to index

--- src/app/planning-poker-store/selectors/index.ts
+++ src/app/planning-poker-store/selectors/index.ts
@@ -4,6 +4,8 @@ import * as SessionSelectors from './session.selectors';
 import * as UserSelectors from './user.selectors';
 import * as TeamSelectors from './team.selectors';
 import * as BacklogSelectors from './backlog.selectors';
+import * as EstimationSelectors from './estimation.selectors';
+import * as DiscussionSelector from './discussion.selectors';
 
 export {
   DecksSelectors,
@@ -11,5 +13,7 @@ export {
   SessionSelectors,
   UserSelectors,
   TeamSelectors,
-  BacklogSelectors
+  BacklogSelectors,
+  EstimationSelectors,
+  DiscussionSelector
 };
