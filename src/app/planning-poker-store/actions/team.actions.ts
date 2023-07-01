5e21205f53c1b23e5952ef2337284e2fc20d929b
seroo7859 <104740228+seroo7859@users.noreply.github.com>
2023-07-01 21:15:12 +0200

    add rename team actions

--- src/app/planning-poker-store/actions/team.actions.ts
+++ src/app/planning-poker-store/actions/team.actions.ts
@@ -1,6 +1,33 @@
 import { createAction, props } from '@ngrx/store';
 import { ActionTypes } from "./planning-poker-store.actions";
-import { TeamMemberModel } from "../../core/models/team.model";
+import { TeamMemberModel, TeamModel } from "../../core/models/team.model";
+import { HttpErrorResponse } from "@angular/common/http";
+
+
+// Rename Team Actions
+
+export const renameTeam = createAction(
+  ActionTypes.RenameTeam,
+  props<{ name: string }>()
+);
+
+export const renameTeamSuccess = createAction(
+  ActionTypes.RenameTeamSuccess,
+  props<{ team: TeamModel }>()
+);
+
+export const renameTeamFailure = createAction(
+  ActionTypes.RenameTeamFailure,
+  props<{ error: HttpErrorResponse }>()
+);
+
+export const teamRenamed = createAction(
+  ActionTypes.TeamRenamed,
+  props<{ name: string }>()
+);
+
+
+// Team Member Actions
 
 export const teamMemberJoined = createAction(
   ActionTypes.TeamMemberJoined,
