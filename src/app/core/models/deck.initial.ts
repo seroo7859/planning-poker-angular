d97e812775697a59f4739dcbe085f839cb66572d
seroo7859 <104740228+seroo7859@users.noreply.github.com>
2023-07-01 21:06:05 +0200

    add deck card initial model

--- src/app/core/models/deck.initial.ts
+++ src/app/core/models/deck.initial.ts
@@ -1,4 +1,4 @@
-import { DeckCardColorsModel } from "./deck.model";
+import { DeckCardModel, DeckCardColorsModel } from "./deck.model";
 
 export const deckCardColorsInitial: DeckCardColorsModel = {
   frontTextColor: '#000000',
@@ -6,3 +6,10 @@ export const deckCardColorsInitial: DeckCardColorsModel = {
   backgroundColor: '#F8F9FA',
   hoverColor: '#FFF0C1'
 }
+
+export const deckCardInitial: DeckCardModel = {
+  value: '?',
+  colors: deckCardColorsInitial,
+  flipped: false,
+  selected: false
+};
