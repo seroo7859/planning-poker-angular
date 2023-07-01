5e7acdc0909d8b430094adcd8cdf14a4fdb5d161
seroo7859 <104740228+seroo7859@users.noreply.github.com>
2023-07-01 21:20:55 +0200

    add rename team to team effects

--- src/app/planning-poker-store/effects/team.effects.ts
+++ src/app/planning-poker-store/effects/team.effects.ts
@@ -2,12 +2,43 @@ import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { ToastService } from "../../core/services/toast.service";
 import { TeamActions } from "../actions";
-import { map } from "rxjs/operators";
+import { catchError, map } from "rxjs/operators";
+import { exhaustMap, of, withLatestFrom } from "rxjs";
+import { SessionSelectors, UserSelectors } from "../selectors";
+import { Store } from "@ngrx/store";
+import { TeamService } from "../../core/services/team.service";
 
 @Injectable()
 export class TeamEffects {
 
-  constructor(private actions$: Actions, private toastService: ToastService) {}
+  constructor(private actions$: Actions, private readonly store: Store, private teamService: TeamService, private toastService: ToastService) {}
+
+  renameTeam$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(TeamActions.renameTeam),
+      withLatestFrom(this.store.select(SessionSelectors.selectSession)),
+      exhaustMap(([action, session]) =>
+        this.teamService.renameTeam(session.data!.id, action.name).pipe(
+          map(team => TeamActions.renameTeamSuccess({ team })),
+          catchError(error => of(TeamActions.renameTeamFailure({ error })))
+        )
+      )
+    )
+  );
+
+  renameTeamFailureToast$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(TeamActions.renameTeamFailure),
+      map(({ error }) => this.toastService.showError({ text: `Rename team failed because ${error.error.message.replace(/^name:/, "").toLowerCase()}`}))
+    );
+  }, { dispatch: false });
+
+  teamRenamedToast$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(TeamActions.teamRenamed),
+      map(({ name }) => this.toastService.showInfo({ text: `Team renamed to '${name.replaceAll(' ', '&nbsp;')}'` }))
+    );
+  }, { dispatch: false });
 
   teamMemberJoined$ = createEffect(() => {
     return this.actions$.pipe(
@@ -21,8 +52,12 @@ export class TeamEffects {
   teamMemberLeaved$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(TeamActions.teamMemberLeaved),
-      map(action => {
-        this.toastService.showInfo({ text: `${action.teamMember.name} leave the session` })
+      withLatestFrom(this.store.select(UserSelectors.selectUser)),
+      map(([{ teamMember }, currentUser]) => {
+        if(teamMember.name === currentUser?.name) {
+          return;
+        }
+        this.toastService.showInfo({ text: `${teamMember.name} leave the session` })
       })
     );
   }, { dispatch: false });
