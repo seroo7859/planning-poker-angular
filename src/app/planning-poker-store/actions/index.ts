175c1ee4e5925003197cf78e6d76fc257dfac74a
seroo7859 <104740228+seroo7859@users.noreply.github.com>
2023-07-01 21:16:53 +0200

    add estimation, discussion and deck actions to index

--- src/app/planning-poker-store/actions/index.ts
+++ src/app/planning-poker-store/actions/index.ts
@@ -1,13 +1,19 @@
 import * as DecksActions from './decks.actions';
+import * as DeckActions from './deck.actions';
 import * as SessionActions from './session.actions';
 import * as UserActions from './user.actions';
 import * as TeamActions from './team.actions';
 import * as BacklogActions from './backlog.actions';
+import * as EstimationActions from './estimation.actions';
+import * as DiscussionActions from './discussion.actions';
 
 export {
   DecksActions,
+  DeckActions,
   SessionActions,
   UserActions,
   TeamActions,
-  BacklogActions
+  BacklogActions,
+  EstimationActions,
+  DiscussionActions
 };
