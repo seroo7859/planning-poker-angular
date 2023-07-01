46dd82848c390bb91459f4cdb8d46a9e1dda8210
seroo7859 <104740228+seroo7859@users.noreply.github.com>
2023-07-01 21:49:23 +0200

    add selectable, custom back side and scrolling to deck card component

--- src/app/core/components/deck/deck-card/deck-card.component.ts
+++ src/app/core/components/deck/deck-card/deck-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output, TemplateRef } from '@angular/core';
 import { DeckCardModel } from "../../../models/deck.model";
 
 @Component({
@@ -12,14 +12,32 @@ export class DeckCardComponent {
   size: 'sm' | 'md' | 'lg' = 'md';
 
   @Input()
+  selectable: boolean = true;
+
+  @Input()
   deckCard?: DeckCardModel;
+
+  @Input()
+  backSide?: TemplateRef<any>;
 
   @Output()
   deckCardSelected = new EventEmitter<DeckCardModel>()
 
+  constructor(private el: ElementRef) {}
+
+  scrollToDeckCard() {
+    this.el.nativeElement.scrollIntoView({ behavior: "smooth", block: 'center', inline: 'nearest' });
+  }
+
   onDeckCardSelected() {
+    if (!this.selectable) {
+      return;
+    }
     if (this.deckCard) {
       this.deckCard.selected = !this.deckCard.selected;
+    }
+    if (this.deckCard?.selected) {
+      this.scrollToDeckCard();
     }
     this.deckCardSelected.emit(this.deckCard);
   }
@@ -29,11 +47,11 @@ export class DeckCardComponent {
       '--deck-card-front-color': this.deckCard?.colors.frontTextColor,
       '--deck-card-front-bg': this.deckCard?.colors.backgroundColor,
       '--deck-card-front-hover-color': this.deckCard?.colors.frontTextColor,
-      '--deck-card-front-hover-bg': this.deckCard?.colors.hoverColor,
+      '--deck-card-front-hover-bg': this.selectable ? this.deckCard?.colors.hoverColor : this.deckCard?.colors.backgroundColor,
       '--deck-card-back-color': this.deckCard?.colors.backTextColor,
       '--deck-card-back-bg': this.deckCard?.colors.backgroundColor,
       '--deck-card-back-hover-color': this.deckCard?.colors.backTextColor,
-      '--deck-card-back-hover-bg': this.deckCard?.colors.hoverColor
+      '--deck-card-back-hover-bg': this.selectable ? this.deckCard?.colors.hoverColor : this.deckCard?.colors.backgroundColor
     };
   }
 
