42d19860c05a8ee2229d94a5664645ba8a6ea5ad
seroo7859 <104740228+seroo7859@users.noreply.github.com>
2023-07-01 21:05:04 +0200

    add estimation and discussion to session model

--- src/app/core/models/session.model.ts
+++ src/app/core/models/session.model.ts
@@ -1,12 +1,17 @@
 import { DeckModel } from "./deck.model";
 import { TeamModel } from "./team.model";
 import { BacklogModel } from "./backlog.model";
+import { EstimationRoundModel, EstimationSummaryModel } from "./estimation.model";
+import {DiscussionModel} from "./discussion.model";
 
 export interface SessionModel {
   id: string;
   team: TeamModel;
   deck: DeckModel;
   backlog: BacklogModel;
+  estimationRound?: EstimationRoundModel;
+  estimationSummary?: EstimationSummaryModel;
+  discussion?: DiscussionModel;
   createdAt: string;
 }
 
