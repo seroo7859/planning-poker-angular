8900effd83e2f0c020609300a55c0bd5b102e68c
seroo7859 <104740228+seroo7859@users.noreply.github.com>
2023-07-01 21:23:05 +0200

    add estimation and deck effects to index

--- src/app/planning-poker-store/effects/index.ts
+++ src/app/planning-poker-store/effects/index.ts
@@ -2,10 +2,14 @@ import * as SessionEffects from './session.effects';
 import * as UserEffects from './user.effects';
 import * as TeamEffects from './team.effects';
 import * as BacklogEffects from './backlog.effects';
+import * as EstimationEffects from './estimation.effects';
+import * as DeckEffects from './deck.effects';
 
 export {
   SessionEffects,
   UserEffects,
   TeamEffects,
-  BacklogEffects
+  BacklogEffects,
+  EstimationEffects,
+  DeckEffects
 };
--- src/app/planning-poker-store/effects/planning-poker-store.effects.ts
+++ src/app/planning-poker-store/effects/planning-poker-store.effects.ts
@@ -1,8 +1,10 @@
-import { BacklogEffects, SessionEffects, TeamEffects, UserEffects } from "./index";
+import { BacklogEffects, SessionEffects, TeamEffects, UserEffects, EstimationEffects, DeckEffects } from "./index";
 
 export const effects = [
   SessionEffects.SessionEffects,
   UserEffects.UserEffects,
   TeamEffects.TeamEffects,
-  BacklogEffects.BacklogEffects
+  BacklogEffects.BacklogEffects,
+  EstimationEffects.EstimationEffects,
+  DeckEffects.DeckEffects
 ];
