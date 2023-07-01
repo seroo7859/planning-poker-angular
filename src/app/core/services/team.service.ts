04fb8f1c032db56435c275dd7e869257acadde7c
seroo7859 <104740228+seroo7859@users.noreply.github.com>
2023-07-01 21:42:25 +0200

    add team service

---
+++ src/app/core/services/team.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { TeamModel } from "../models/team.model";
+import { Observable } from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TeamService {
+
+  // URL to Team-API
+  private url = 'api/team';
+
+  // HTTP Options
+  private httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor(private http: HttpClient) { }
+
+  /**
+   * Get the team on the session.
+   * @param sessionId
+   */
+  getTeam(sessionId: string): Observable<TeamModel> {
+    return this.http.get<TeamModel>(`${this.url}/${sessionId}`, this.httpOptions);
+  }
+
+  /**
+   * Rename the team on the session.
+   * @param sessionId
+   * @param newName
+   */
+  renameTeam(sessionId: string, newName: string): Observable<TeamModel> {
+    return this.http.patch<TeamModel>(`${this.url}/rename?sessionId=${sessionId}`, { name: newName }, this.httpOptions);
+  }
+
+}
