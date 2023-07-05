9fa2abea1da7648e38324bcd82369d10decb8b2a
seroo7859 <104740228+seroo7859@users.noreply.github.com>
2023-07-05 10:36:45 +0200

    add discussion service

---
+++ src/app/core/services/discussion.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { DiscussionModel, DiscussionPostCreateModel, DiscussionPostModel } from "../models/discussion.model";
+import { Observable } from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DiscussionService {
+
+  // URL to Discussion-API
+  private url = 'api/discussion';
+
+  // HTTP Options
+  private httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor(private http: HttpClient) { }
+
+  /**
+   * Get the discussion in the session.
+   * @param sessionId
+   */
+  getDiscussion(sessionId: string): Observable<DiscussionModel> {
+    return this.http.get<DiscussionModel>(`${this.url}?sessionId=${sessionId}`, this.httpOptions);
+  }
+
+  /**
+   * Start a discussion in the session.
+   * @param sessionId
+   * @param topic
+   */
+  startDiscussion(sessionId: string, topic: string): Observable<DiscussionModel> {
+    return this.http.post<DiscussionModel>(`${this.url}/start?sessionId=${sessionId}`, { topic }, this.httpOptions);
+  }
+
+  /**
+   * End a discussion in the session.
+   * @param sessionId
+   */
+  endDiscussion(sessionId: string): Observable<DiscussionModel> {
+    return this.http.post<DiscussionModel>(`${this.url}/end?sessionId=${sessionId}`, { }, this.httpOptions);
+  }
+
+  /**
+   * Create a discussion post in the session.
+   * @param sessionId
+   * @param newDiscussionPost
+   */
+  createDiscussionPost(sessionId: string, newDiscussionPost: DiscussionPostCreateModel): Observable<DiscussionPostModel> {
+    return this.http.post<DiscussionPostModel>(`${this.url}/post?sessionId=${sessionId}`, newDiscussionPost, this.httpOptions);
+  }
+
+}
